a275a2835c4d4a1ce3fb784b5f0ef71f4e3c2162
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-07 04:50:47 +0200

    *edit* front / main page : adding service provider + paginable / suspense component integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { Suspense, useEffect, useRef } from 'react'
 import './App.css'
+import { useServices } from './context/useServices'
 import { EmployeeCard } from './pagination/EmployeeCard'
-import { employees } from './pagination/Employees'
+import { employees } from './pagination/constants/Employees'
 import Paginable from './pagination/Paginable'
+import Component from './fetch-withSuspense/Component'
+import { ErrorBoundary } from 'react-error-boundary'
 
 function App() {
 
-  return (
-    <>
-      <Paginable items={employees} Renderer={EmployeeCard} perPage={4} className='flex flex-row gap-x-[10px]'/>
-    </>
-  )
+    const {service1, service2} = useServices()
+
+    const firstRender = useRef<boolean>(true)
+
+    useEffect(() => {
+        if(firstRender.current == false) return
+        console.log(service1.test())
+        console.log(service2.test())
+        firstRender.current = false
+    }, [])
+
+    return (
+        <>
+            <Paginable items={employees} Renderer={EmployeeCard} perPage={4} className='flex flex-row gap-x-[10px]'/>
+            <ErrorBoundary fallback={<div>Error!</div>}>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Component/>
+                </Suspense>
+            </ErrorBoundary>
+        </>
+    )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ServicesProvider } from './context/ServicesContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ServicesProvider>
+            <App />
+        </ServicesProvider>
+    </StrictMode>,
 )
