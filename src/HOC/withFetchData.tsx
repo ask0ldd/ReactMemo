92b3d356e8e096bc57d1cc230946af714f4f8ca1
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-22 16:04:21 +0200

    *add* front / HOC : withFetchData

---
+++ src/HOC/withFetchData.tsx
@@ -0,0 +1,40 @@
+import { ComponentType, useEffect, useState } from "react";
+
+interface InjectedFetchProps<T> {
+    data: T | null
+    isLoading: boolean
+    error: Error | null
+}
+
+export function withDataFetch<GWComponentProps, GData>(
+    // React.ComponentType<P> means "a React component that accepts props of type P"
+    WrappedComponent : ComponentType<GWComponentProps & InjectedFetchProps<GData>>, 
+    fetchData : () => Promise<GData> 
+) {
+    return function EnhancedComponent(props : Omit<GWComponentProps, keyof InjectedFetchProps<GData>>) {
+        const [data, setData] = useState<GData|null>(null)
+        const [isLoading, setIsLoading] = useState(true)
+        const [error, setError] = useState<Error | null>(null)
+  
+        useEffect(() => {
+            fetchData()
+            .then(response => {
+                setData(response)
+                setIsLoading(false)
+            })
+            .catch(err => {
+                setError(err)
+                setIsLoading(false)
+            })
+        }, [])
+  
+        return (
+            <WrappedComponent
+                {...props as GWComponentProps}
+                data={data}
+                isLoading={isLoading}
+                error={error}
+            />
+        )
+    }
+}
