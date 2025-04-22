f53ef638b2fbadfd84686d661fbe58822cc8eef3
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-22 19:13:31 +0200

    *add* front / render props component example : mouse tracker

---
+++ src/renderProps/MouseTracker.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useState } from "react";
+
+// The MouseTracker component encapsulates the logic for tracking mouse position.
+export default function MouseTracker ({ render } : { render : ({ x, y} : {x : number, y : number}) => ReactNode }) {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+  
+    function handleMouseMove (event : React.MouseEvent) {
+      setPosition({
+        x: event.clientX,
+        y: event.clientY
+      });
+    };
+  
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={handleMouseMove}>
+        {render(position)}
+      </div>
+    );
+};
