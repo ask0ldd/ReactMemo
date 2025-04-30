46e769206076b850acda956dd2ddaaf53f4b7e2e
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-30 02:08:27 +0200

    *edit* / render Props pattern : mouse tracker

--- src/renderProps/MouseTracker.tsx
+++ src/renderProps/MouseTracker.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 
 // The MouseTracker component encapsulates the logic for tracking mouse position.
+// Takes a render callback and returns a ReactNode
 export default function MouseTracker ({ render } : { render : ({ x, y} : {x : number, y : number}) => ReactNode }) {
     const [position, setPosition] = useState({ x: 0, y: 0 })
   
--- src/renderProps/PageUsingMouseTracker.tsx
+++ src/renderProps/PageUsingMouseTracker.tsx
@@ -1,6 +1,6 @@
 import MouseTracker from "./MouseTracker";
 
-export default function PageRenderProps(){
+export default function PageUsingMouseTracker(){
     return(
         <MouseTracker render={({ x, y }) => (
             <h1>The mouse position is ({x}, {y})</h1>
